44c27173d84462891835452895a24f2284c5a775
root <724590161@qq.com>
2022-01-09 14:31:44 +0800

    初版上git

---
+++ src/main/java/yuan/cam/eureka2/EruekaServerApplication.java
@@ -0,0 +1,16 @@
+package yuan.cam.eureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EruekaServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EruekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yuan/cam/eureka2/EruekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package yuan.cam.eureka2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EruekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
